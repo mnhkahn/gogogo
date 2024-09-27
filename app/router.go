2943e698f8d8d4d5bf7633eb94ae59a53f9c3408
mnhkahn <lichao@cyeam.com>
2024-09-27 21:13:06 +0800

    sitemap

--- app/router.go
+++ app/router.go
@@ -7,6 +7,6 @@ func InitRouter() {
 	Handle("/debug/router", BasicAuthHandler(&Got{H: DebugRouter}, usr, pwd))
 	Handle("/debug/log/level", BasicAuthHandler(Got{LogLevelHandler}, usr, pwd))
 	Handle("/debug/stat", BasicAuthHandler(Got{StatHandler}, usr, pwd))
-	// Handle("/sitemap.xml", Got{SiteMapXML})
+	Handle("/sitemap.xml", Got{SiteMapXML})
 	Handle("/sitemap", Got{SiteMapRaw})
 }
